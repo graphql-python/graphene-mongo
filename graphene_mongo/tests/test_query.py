bd2d6c4756218bd6ab45b1e9bf323f2e8525b0e8
abawchen <abaw.chen@gmail.com>
2018-01-29 13:28:09 +0800

    test: Add test_query.test_should_mutate_well

--- graphene_mongo/tests/test_query.py
+++ graphene_mongo/tests/test_query.py
@@ -226,9 +226,61 @@ def test_should_connection_field():
     assert not result.errors
     assert dict(result.data['allEditors']) == expected['allEditors']
 
-# TODO:
+
 def test_should_mutate_well():
-    pass
+    class ArticleNode(MongoengineObjectType):
+
+        class Meta:
+            model = Article
+            interfaces = (Node,)
+
+
+    class CreateArticle(graphene.Mutation):
+
+        class Arguments:
+            headline = graphene.String()
+
+        article = graphene.Field(ArticleNode)
+
+        def mutate(self, info, headline):
+            article = Article(
+                headline=headline
+            )
+            article.save()
+
+            return CreateArticle(article=article)
+
+
+    class Query(graphene.ObjectType):
+        node = Node.Field()
+
+
+    class Mutation(graphene.ObjectType):
+
+        create_article = CreateArticle.Field()
+
+    query = '''
+        mutation ArticleCreator {
+            createArticle(
+                headline: "My Article"
+            ) {
+                article {
+                    headline
+                }
+            }
+        }
+    '''
+    expected = {
+        'createArticle': {
+            'article': {
+                'headline': 'My Article'
+            }
+        }
+    }
+    schema = graphene.Schema(query=Query, mutation=Mutation)
+    result = schema.execute(query)
+    assert not result.errors
+    assert result.data == expected
 
 # TODO:
 def test_should_filter():
