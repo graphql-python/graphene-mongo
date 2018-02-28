ff742368aa6c8a6ba2ece6f58d192b4c60983205
abawchen <abaw.chen@gmail.com>
2018-02-28 23:55:12 +0800

    refactor: Separate relay test case.

---
+++ graphene_mongo/tests/fixtures.py
@@ -0,0 +1,34 @@
+from .models import Article, Editor, Player, Reporter
+
+
+def setup_fixtures():
+    print('setup_fixtures' * 10)
+    editor1 = Editor(first_name='Penny', last_name='Hardaway')
+    editor1.save()
+    editor2 = Editor(first_name='Grant', last_name='Hill')
+    editor2.save()
+    editor3 = Editor(first_name='Dennis', last_name='Rodman')
+    editor3.save()
+
+    reporter = Reporter(first_name='Allen', last_name='Iverson',
+                        email='ai@gmail.com',  awards=['2010-mvp'])
+    article1 = Article(headline='Hello', editor=editor1)
+    article1.save()
+    article2 = Article(headline='World', editor=editor2)
+    article2.save()
+
+    reporter.articles = [article1, article2]
+    reporter.save()
+
+    player1 = Player(first_name='Michael', last_name='Jordan')
+    player1.save()
+    player2 = Player(first_name='Magic', last_name='Johnson', opponent=player1)
+    player2.save()
+    player3 = Player(first_name='Larry', last_name='Bird', players=[player1, player2])
+    player3.save()
+
+    player1.players = [player2]
+    player1.save()
+
+    player2.players = [player1]
+    player2.save()
