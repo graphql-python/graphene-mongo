4b4598ead2e565b8f4048e795273f1c4e3ae8850
abawchen <abaw.chen@gmail.com>
2018-04-02 18:05:34 +0800

    feat: Use model._fields directly.

--- graphene_mongo/tests/test_utils.py
+++ graphene_mongo/tests/test_utils.py
@@ -5,13 +5,17 @@ from mongoengine import Document
 from ..utils import (
     get_model_fields, is_valid_mongoengine_model
 )
-from .models import Reporter
+from .models import Reporter, Child
 
 def test_get_model_fields_no_duplication():
     reporter_fields = get_model_fields(Reporter)
     reporter_name_set = set(reporter_fields)
     assert len(reporter_fields) == len(reporter_name_set)
 
+
+def test_get_base_model_fields():
+    child_fields = get_model_fields(Child)
+    assert all(field in set(child_fields) for field in ['bar', 'baz'])
 
 def test_is_valid_mongoengine_mode():
     assert is_valid_mongoengine_model(Reporter)
