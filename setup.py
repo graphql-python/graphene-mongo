f2ce8925e505c30e9ea7cfdefdca9cd427520c7f
abawchen <abaw.chen@gmail.com>
2018-04-02 21:56:19 +0800

    chore: Bump version to 0.1.10 :hankey:

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='graphene-mongo',
-    version='0.1.9',
+    version='0.1.10',
 
     description='Graphene Mongoengine integration',
     # long_description=open('README.rst').read(),
