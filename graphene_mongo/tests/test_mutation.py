3623876469b6de7a0952d108a342ec385a3a6944
AbawChen <abaw.chen@gmail.com>
2018-06-02 11:40:36 +0800

    test: Add test_should_update

--- graphene_mongo/tests/test_mutation.py
+++ graphene_mongo/tests/test_mutation.py
@@ -2,10 +2,15 @@ import graphene
 
 from graphene.relay import Node
 
-from .models import (Article,)
-from .types import (ArticleNode,)
+from .fixtures import setup_fixtures
+from .models import (Article, Editor)
+from .types import (ArticleNode, EditorNode)
 
-def test_should_mutate_create():
+
+setup_fixtures()
+
+
+def test_should_create():
 
     class CreateArticle(graphene.Mutation):
 
@@ -54,3 +59,53 @@ def test_should_mutate_create():
     assert not result.errors
     assert result.data == expected
 
+
+def test_should_update():
+
+    class UpdateEditor(graphene.Mutation):
+
+        class Arguments:
+            id = graphene.ID()
+            first_name = graphene.String()
+
+        editor = graphene.Field(EditorNode)
+
+        def mutate(self, info, id, first_name):
+            print(id, first_name)
+            editor = Editor.objects.get(id=id)
+            editor.first_name = first_name
+            editor.save()
+            return UpdateEditor(editor=editor)
+
+    class Query(graphene.ObjectType):
+
+        node = Node.Field()
+
+    class Mutation(graphene.ObjectType):
+
+        update_editor = UpdateEditor.Field()
+
+    query = '''
+        mutation EditorUpdater {
+            updateEditor(
+                id: "1"
+                firstName: "Tony"
+            ) {
+                editor {
+                    firstName
+                }
+            }
+        }
+    '''
+    expected = {
+        'updateEditor': {
+            'editor': {
+                'firstName': 'Tony'
+            }
+        }
+    }
+    schema = graphene.Schema(query=Query, mutation=Mutation)
+    result = schema.execute(query)
+    # print(result.data)
+    assert not result.errors
+    assert result.data == expected
