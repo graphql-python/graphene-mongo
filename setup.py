3585ad30bbfab56dce7b2487fd57121467126d3a
hw <keyeMyria@users.noreply.github.com>
2018-06-03 22:33:53 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='graphene-mongo',
-    version='0.1.12',
+    version='0.1.13',
 
     description='Graphene Mongoengine integration',
     long_description=open('README.rst').read(),
