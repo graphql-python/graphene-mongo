0132d5546645cd4052c91ce81d9f08fa75dd2968
hw <keyeMyria@users.noreply.github.com>
2018-06-07 22:51:33 +0800

    add LongField converter test
    add test for Mongoengine LongField Converter to graphene.Int

--- graphene_mongo/tests/test_converter.py
+++ graphene_mongo/tests/test_converter.py
@@ -50,6 +50,10 @@ def test_sould_int_convert_int():
     assert_conversion(mongoengine.IntField, graphene.Int)
 
 
+def test_sould_long_convert_int():
+    assert_conversion(mongoengine.LongField, graphene.Int)
+    
+
 def test_should_object_id_convert_id():
     assert_conversion(mongoengine.ObjectIdField, graphene.ID)
 
