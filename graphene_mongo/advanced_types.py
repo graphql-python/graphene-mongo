c91a06460d58154e2a33f448eb4eb74e97d9259c
AbawChen <abaw.chen@gmail.com>
2019-01-17 09:11:14 +0800

    feat: Add advanced_types.

---
+++ graphene_mongo/advanced_types.py
@@ -0,0 +1,15 @@
+import graphene
+
+
+def _resolve_point_type_coordinates(self, info):
+    return self['coordinates']
+
+
+class PointType(graphene.ObjectType):
+
+    type = graphene.String()
+    coordinates = graphene.List(
+        graphene.Float, resolver=_resolve_point_type_coordinates)
+
+    def resolve_type(self, info):
+        return self['type']
