98ff0baa508d0a652afbaea501a34bc88b86b585
Arun S Kumar <asuresh960@gmail.com>
2019-05-08 13:10:52 +0530

    Update fields.py

--- graphene_mongo/fields.py
+++ graphene_mongo/fields.py
@@ -8,7 +8,7 @@ import mongoengine
 from graphene.relay import ConnectionField
 from graphene.types.argument import to_arguments
 from graphene.types.dynamic import Dynamic
-from graphene.types.structures import Structure, List
+from graphene.types.structures import Structure
 from graphql_relay.connection.arrayconnection import connection_from_list_slice
 
 from .advanced_types import (
