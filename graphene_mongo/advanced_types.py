52076602698f2630c44a2596afad9347a0334c54
AbawChen <abaw.chen@gmail.com>
2019-05-13 10:42:16 +0800

    refactor: Reduce some codes

--- graphene_mongo/advanced_types.py
+++ graphene_mongo/advanced_types.py
@@ -11,37 +11,32 @@ def _resolve_fs_field(field, name, default_value=None):
     return getattr(v, name, default_value)
 
 
-def _resolve_content_type(self, info):
-    return _resolve_fs_field(self, 'content_type')
-
-
-def _resolve_md5(self, info):
-    return _resolve_fs_field(self, 'md5')
-
-
-def _resolve_chunk_size(self, info):
-    return _resolve_fs_field(self, 'chunk_size', 0)
-
-
-def _resolve_length(self, info):
-    return _resolve_fs_field(self, 'length', 0)
-
-
-def _resolve_data(self, info):
-    v = getattr(self.instance, self.key)
-    data = v.read()
-    if data is not None:
-        return base64.b64encode(data)
-    return None
-
-
 class FileFieldType(graphene.ObjectType):
 
-    content_type = graphene.String(resolver=_resolve_content_type)
-    md5 = graphene.String(resolver=_resolve_md5)
-    chunk_size = graphene.Int(resolver=_resolve_chunk_size)
-    length = graphene.Int(resolver=_resolve_length)
-    data = graphene.String(resolver=_resolve_data)
+    content_type = graphene.String()
+    md5 = graphene.String()
+    chunk_size = graphene.Int()
+    length = graphene.Int()
+    data = graphene.String()
+
+    def resolve_content_type(self, info):
+        return _resolve_fs_field(self, 'content_type')
+
+    def resolve_md5(self, info):
+        return _resolve_fs_field(self, 'md5')
+
+    def resolve_chunk_size(self, info):
+        return _resolve_fs_field(self, 'chunk_size', 0)
+
+    def resolve_length(self, info):
+        return _resolve_fs_field(self, 'length', 0)
+
+    def resolve_data(self, info):
+        v = getattr(self.instance, self.key)
+        data = v.read()
+        if data is not None:
+            return base64.b64encode(data)
+        return None
 
 
 class _TypeField(graphene.ObjectType):
