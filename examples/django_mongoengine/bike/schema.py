d8916227624f91bbdea853dddae5b3a8cd726520
Eduard Erja <eduard.erja@gmail.com>
2019-07-19 19:40:08 +0300

    Added new model class without relay

--- examples/django_mongoengine/bike/schema.py
+++ examples/django_mongoengine/bike/schema.py
@@ -1,12 +1,17 @@
 import graphene
 from graphene.relay import Node
 from graphene_mongo.fields import MongoengineConnectionField
-from .types import BikeType
+from .models import Shop
+from .types import BikeType, ShopType
 
 
 class Query(graphene.ObjectType):
     node = Node.Field()
     bikes = MongoengineConnectionField(BikeType)
+    shop_list = graphene.List(ShopType)
+
+    def resolve_shop_list(self, info):
+        return Shop.objects.all()
 
 
 schema = graphene.Schema(query=Query, types=[BikeType, ])
