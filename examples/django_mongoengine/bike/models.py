3983ec6467c68f037483540e518e95f69c06f762
Eduard Erja <eduard.erja@gmail.com>
2019-07-19 20:41:20 +0300

    Added crud mutations and unittesting

--- examples/django_mongoengine/bike/models.py
+++ examples/django_mongoengine/bike/models.py
@@ -1,9 +1,13 @@
 from mongoengine import Document
-from mongoengine.fields import FloatField, StringField, ListField, URLField
+from mongoengine.fields import (
+    FloatField, StringField,
+    ListField, URLField, ObjectIdField
+)
 
 
 class Shop(Document):
     meta = {'collection': 'shop'}
+    ID = ObjectIdField()
     name = StringField()
     address = StringField()
     website = URLField()
@@ -11,6 +15,7 @@ class Shop(Document):
 
 class Bike(Document):
     meta = {'collection': 'bike'}
+    ID = ObjectIdField()
     name = StringField()
     brand = StringField()
     year = StringField()
--- examples/django_mongoengine/bike/urls.py
+++ examples/django_mongoengine/bike/urls.py
@@ -3,5 +3,5 @@ from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
-    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True)), name='graphql-query'),
 ]
