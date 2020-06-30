7ca0925bd2865cee85a38a4db1e75349f85595f3
abawchen <abaw.chen@gmail.com>
2020-06-30 17:29:31 +0800

    chore: bump version to 0.2.13

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="graphene-mongo",
-    version="0.2.12",
+    version="0.2.13",
     description="Graphene Mongoengine integration",
     long_description=open("README.rst").read(),
     url="https://github.com/graphql-python/graphene-mongo",
