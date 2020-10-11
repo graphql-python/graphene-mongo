9960564ea27d7fd0c664f51ccff112697af7eedb
Jonathan Ehwald <github@ehwald.info>
2020-10-11 16:55:45 +0200

    Bump graphene dependency to v3.0.0b5

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     keywords="api graphql protocol rest relay graphene mongo mongoengine",
     packages=find_packages(exclude=["tests"]),
     install_requires=[
-        "graphene>=2.1.3,<3",
+        "graphene>=v3.0.0b5",
         "mongoengine>=0.15.0",
         "singledispatch>=3.4.0.3",
         "iso8601>=0.1.12",
