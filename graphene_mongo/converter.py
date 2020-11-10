28cd0fdd2e83d2cbeac831b8acd068be23161552
Arun S Kumar <asuresh960@gmail.com>
2020-11-10 13:46:31 +0530

    Bug fix - generic reference and generic lazy resolvers

--- graphene_mongo/converter.py
+++ graphene_mongo/converter.py
@@ -242,7 +242,7 @@ def convert_field_to_union(field, registry=None):
             querying_types = list(get_query_fields(args[0]).keys())
             if _type.__name__ in querying_types:
                 queried_fields = list()
-                for each in get_query_fields(args[0]).keys():
+                for each in get_query_fields(args[0])[_type._meta.name].keys():
                     item = to_snake_case(each)
                     if item in document._fields_ordered + tuple(filter_args):
                         queried_fields.append(item)
@@ -252,7 +252,42 @@ def convert_field_to_union(field, registry=None):
             return document
         return None
 
-    if isinstance(field, mongoengine.GenericReferenceField):
+    def lazy_reference_resolver(root, *args, **kwargs):
+        document = getattr(root, field.name or field.db_name)
+        if document:
+            queried_fields = list()
+            document_field_type = registry.get_type_for_model(document.document_type)
+            querying_types = list(get_query_fields(args[0]).keys())
+            filter_args = list()
+            if document_field_type._meta.filter_fields:
+                for key, values in document_field_type._meta.filter_fields.items():
+                    for each in values:
+                        filter_args.append(key + "__" + each)
+            if document_field_type._meta.name in querying_types:
+                for each in get_query_fields(args[0])[document_field_type._meta.name].keys():
+                    item = to_snake_case(each)
+                    if item in document.document_type._fields_ordered + tuple(filter_args):
+                        queried_fields.append(item)
+                _type = registry.get_type_for_model(document.document_type)
+                return document.document_type.objects().no_dereference().only(
+                    *(set((list(_type._meta.required_fields) + queried_fields)))).get(
+                    pk=document.pk)
+            return document.document_type()
+        return None
+
+    if isinstance(field, mongoengine.GenericLazyReferenceField):
+        field_resolver = None
+        required = False
+        if field.db_field is not None:
+            required = field.required
+            resolver_function = getattr(registry.get_type_for_model(field.owner_document), "resolve_" + field.db_field,
+                                        None)
+            if resolver_function and callable(resolver_function):
+                field_resolver = resolver_function
+        return graphene.Field(_union, resolver=field_resolver if field_resolver else lazy_reference_resolver,
+                              description=get_field_description(field, registry), required=required)
+
+    elif isinstance(field, mongoengine.GenericReferenceField):
         field_resolver = None
         required = False
         if field.db_field is not None:
