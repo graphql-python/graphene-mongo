224f6551374e84150864bd2e75d9d01a224f5809
BenoitToulet <benoit.toulet@gmail.com>
2020-12-23 11:38:39 +0100

    version="0.3.1"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="graphene-mongo",
-    version="0.3.0",
+    version="0.3.1",
     description="Graphene Mongoengine integration",
     long_description=open("README.rst").read(),
     url="https://github.com/graphql-python/graphene-mongo",
