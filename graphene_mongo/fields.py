faf24332e487975bf7acb82b823559d6588348e0
Abhinand C <abhinandc.ac@gmail.com>
2023-03-20 20:29:04 +0530

    refactor(Fields): Fixes lint issues, logs error

--- graphene_mongo/fields.py
+++ graphene_mongo/fields.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+import logging
 from collections import OrderedDict
 from functools import partial, reduce
 
@@ -512,9 +513,8 @@ class MongoengineConnectionField(ConnectionField):
                     args.update(resolved._query)
                     args_copy = args.copy()
                     for arg_name, arg in args.copy().items():
-                        if "." in arg_name or arg_name not in self.model._fields_ordered + (
-                                'first', 'last', 'before', 'after') + tuple(
-                            self.filter_args.keys()):
+                        if "." in arg_name or arg_name not in self.model._fields_ordered \
+                                + ('first', 'last', 'before', 'after') + tuple(self.filter_args.keys()):
                             args_copy.pop(arg_name)
                             if arg_name == '_id' and isinstance(arg, dict):
                                 operation = list(arg.keys())[0]
@@ -550,7 +550,7 @@ class MongoengineConnectionField(ConnectionField):
                     try:
                         setattr(root, key, from_global_id(value)[1])
                     except Exception as error:
-                        pass
+                        logging.error("Exception Occurred: ", exc_info=error)
         iterable = resolver(root, info, **args)
 
         if isinstance(connection_type, graphene.NonNull):
