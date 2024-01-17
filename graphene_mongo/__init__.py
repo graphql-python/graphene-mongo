ea00ee22e46eee8125a10acf1ae1e86557730ca7
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2024-01-17 10:27:03 +0530

    bump: version 0.4.2

--- graphene_mongo/__init__.py
+++ graphene_mongo/__init__.py
@@ -1,10 +1,9 @@
 from .fields import MongoengineConnectionField
 from .fields_async import AsyncMongoengineConnectionField
-
-from .types import MongoengineObjectType, MongoengineInputType, MongoengineInterfaceType
+from .types import MongoengineInputType, MongoengineInterfaceType, MongoengineObjectType
 from .types_async import AsyncMongoengineObjectType
 
-__version__ = "0.1.1"
+__version__ = "0.4.2"
 
 __all__ = [
     "__version__",
